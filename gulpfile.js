b5f2ffd4891bf04a92bf8211e6083b06d56a8d62
sumeng <ex-sumeng001@pingan.com.cn>
2020-06-21 16:47:53 +0800

    feat: +gulp配置

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+
+/* 初始化
+function defaultTask(cb) {
+  // place code for your default task here
+  cb();
+}
+
+  
+exports.default = defaultTask
+以上写法👆 执行gulp即可*/
+
+// todo
